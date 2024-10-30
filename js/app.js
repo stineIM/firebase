e4a95c429f8bbb222afc04049f9eb06a375b9b64
Stine <93541692+stineIM@users.noreply.github.com>
2024-10-30 09:55:57 +0100

    added getitems

--- js/app.js
+++ js/app.js
@@ -1,27 +1,51 @@
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: [DIN INFO HER] ,
-    authDomain: [DIN INFO HER] ,
-    projectId: [DIN INFO HER] ,
-    storageBucket: [DIN INFO HER] ,
-    messagingSenderId: [DIN INFO HER] ,
-    appId: [DIN INFO HER] 
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 });
 ///////////////////////////////////////////////////////////
 
 /* Firebase config */
 const db = firebaseApp.firestore();
-let docid = "";
 
+// Legger til element i collection "movies"
 function addItem() {
     const title = document.getElementById("title").value;
     const year = document.getElementById("year").value;
-    console.log("hei")
+    const image = document.getElementById("image").value;
+
     db.collection("movies").doc(title).set({
         title: title,
-        year: year
-    })
+        year: year,
+        image: image
+    }).then(() => {
+        getItems(); // Hent filmene på nytt etter at en ny film er lagt til
+    });
+
     document.getElementById("title").value = ""; 
     document.getElementById("year").value = ""; 
+    document.getElementById("image").value = ""; 
 }
 
+// Henter ut data frå collection som heiter movies. Feltene image, title og year er i kvart dokument i databasen. 
+function getItems() {
+    let items = "";
+    db.collection("movies").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            items += "<div class='movie-card'>" +
+                "<img src='" + data.image + "' alt='" + data.title + "'>" +
+                "<div>" +
+                "<h2>" + data.title + "</h2>" +
+                "<p>År: " + data.year + "</p>" +
+                "</div>" +
+                "</div>";
+        });
+        document.getElementById("itemTable").innerHTML = items;
+    });
+}
+getItems();
